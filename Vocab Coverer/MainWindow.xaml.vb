5ebdb7965ce34df6ec67d17029133d0f4a7824fc
Matthew Grove <matthew@grove.gq>
2019-05-18 22:26:30 +0100

    Add code

---
+++ Vocab Coverer/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+    Sub Window_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Me.MouseDown
+        If e.ChangedButton = MouseButton.Left Then
+            DragMove()
+        End If
+    End Sub
+End Class
